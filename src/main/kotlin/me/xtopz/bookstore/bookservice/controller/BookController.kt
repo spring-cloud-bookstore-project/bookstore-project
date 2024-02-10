a827cecfdbb921b752b1a39b5c98b63814454394
Dmitry <xtopz1337@gmail.com>
2024-02-10 15:18:44 +0300

    edit: add about end-point

--- src/main/kotlin/me/xtopz/bookstore/bookservice/controller/BookController.kt
+++ src/main/kotlin/me/xtopz/bookstore/bookservice/controller/BookController.kt
@@ -1,20 +1,30 @@
 package me.xtopz.bookstore.bookservice.controller
 
+import me.xtopz.bookstore.bookservice.service.BookService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Locale
 
 @RestController
 @RequestMapping("/books")
 class BookController {
 
-    @Value("\${hello-message}")
-    private lateinit var helloMessage: String
+    private lateinit var bookService: BookService
 
-    @GetMapping("/")
-    fun splash(): String {
-        return helloMessage
+    @GetMapping("/about/{id}")
+    fun about(
+        @PathVariable id: Long,
+        @RequestHeader(
+            name = "Accept-Language",
+            required = false
+        ) locale: Locale
+
+    ): String {
+        return bookService.representBook(id, locale)
     }
 
 }
