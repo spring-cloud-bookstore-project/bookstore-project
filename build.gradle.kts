0ff551fa58ea41f4f5408b94a64db3f841385ec6
Dmitry <xtopz1337@gmail.com>
2024-02-10 15:18:17 +0300

    add: kotlin jpa plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.jetbrains.kotlin.plugin.jpa") version "2.0.0-Beta3"
+
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
 }
