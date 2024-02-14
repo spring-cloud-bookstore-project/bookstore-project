06be9e2e30f8b6be87de6a568cad828525673890
Dmitry <xtopz1337@gmail.com>
2024-02-14 23:38:08 +0300

    add: findBookById and findAllBooks

--- src/main/kotlin/me/xtopz/bookstore/bookservice/service/BookService.kt
+++ src/main/kotlin/me/xtopz/bookstore/bookservice/service/BookService.kt
@@ -1,26 +1,23 @@
 package me.xtopz.bookstore.bookservice.service
 
+import me.xtopz.bookstore.bookservice.entity.Book
 import me.xtopz.bookstore.bookservice.repository.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.MessageSource
 import org.springframework.stereotype.Service
-import java.util.Locale
 import kotlin.jvm.optionals.getOrNull
 
 @Service
 class BookService {
 
     @Autowired
-    private lateinit var messageSource: MessageSource
-
-    @Autowired
     private lateinit var bookRepository: BookRepository
 
-    fun representBook(bookId: Long, locale: Locale?): String {
-        val desiredBook = bookRepository.findById(bookId).getOrNull()
-            ?: return messageSource.getMessage("book.not-found", null, locale)
+    fun findBookById(id: Long): Book? {
+        return bookRepository.findById(id).getOrNull()
+    }
 
-        return messageSource.getMessage("book.represent", arrayOf(desiredBook), locale)
+    fun findAllBooks(): List<Book> {
+        return bookRepository.findAll()
     }
 
 }
