8cf10235bd77741d6f04d7ea1fbdb3370fc46c9b
Dmitry <xtopz1337@gmail.com>
2024-02-14 23:40:11 +0300

    add: GET books and GET books/{id} endpoints

--- src/main/kotlin/me/xtopz/bookstore/bookservice/controller/BookController.kt
+++ src/main/kotlin/me/xtopz/bookstore/bookservice/controller/BookController.kt
@@ -1,30 +1,30 @@
 package me.xtopz.bookstore.bookservice.controller
 
 import me.xtopz.bookstore.bookservice.service.BookService
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.Locale
 
 @RestController
 @RequestMapping("/books")
 class BookController {
 
+    @Autowired
     private lateinit var bookService: BookService
 
-    @GetMapping("/about/{id}")
-    fun about(
-        @PathVariable id: Long,
-        @RequestHeader(
-            name = "Accept-Language",
-            required = false
-        ) locale: Locale?
+    @GetMapping("/")
+    fun getBooks() =
+        bookService.findAllBooks()
 
-    ): String {
-        return bookService.representBook(id, locale)
+    @GetMapping("/{id}")
+    fun getBook(@PathVariable id: Long): ResponseEntity<Any> {
+        val desiredBook = bookService.findBookById(id) ?:
+            return ResponseEntity.notFound().build()
+
+        return ResponseEntity.ok(desiredBook)
     }
 
 }
