8c641e8022de4b9e477f768e8bf4f8d9f6309ead
Dmitry <xtopz1337@gmail.com>
2024-02-16 22:05:04 +0300

    changes: add include submodules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
+
 rootProject.name = "bookstore-project"
+
+include(
+    "modules:book-service",
+    "modules:helloworld-project"
+)
 
